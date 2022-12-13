bf55357e48e5b4bcfff3a3df2d8deb47b955ad0a
Tomasz Kłak <tomasz@tomaszklak.pl>
2022-12-13 10:49:17 +0100

    day13 cleanup

--- src/day13.rs
+++ src/day13.rs
@@ -2,43 +2,48 @@ use anyhow::Result;
 use std::cmp::Ordering;
 use std::time::{Duration, Instant};
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Eq)]
 enum Packet {
     Num(u8),
     List(Vec<Packet>),
 }
 
-fn in_right_order(l: &Packet, r: &Packet) -> Option<bool> {
-    use Packet::*;
-    match (l, r) {
-        (Num(l), Num(r)) => match l.cmp(r) {
-            Ordering::Less => Some(true),
-            Ordering::Equal => None,
-            Ordering::Greater => Some(false),
-        },
-        (List(l), List(r)) => in_right_order_list(l, r),
-        (Num(_), List(r)) => in_right_order_list(&[l.clone()], r),
-        (List(l), Num(_)) => in_right_order_list(l, &[r.clone()]),
+use Packet::*;
+
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        fn in_right_order_list(l: &[Packet], r: &[Packet]) -> Ordering {
+            let mut i = 0;
+            loop {
+                if i >= l.len() && i >= r.len() {
+                    return Ordering::Equal;
+                }
+                if i >= l.len() {
+                    return Ordering::Less;
+                }
+                if i >= r.len() {
+                    return Ordering::Greater;
+                }
+                match l[i].cmp(&r[i]) {
+                    Ordering::Equal => {}
+                    o => return o,
+                }
+                i += 1
+            }
+        }
+
+        match (self, other) {
+            (Num(l), Num(r)) => l.cmp(r),
+            (List(l), List(r)) => in_right_order_list(l, r),
+            (Num(_), List(r)) => in_right_order_list(&[self.clone()], r),
+            (List(l), Num(_)) => in_right_order_list(l, &[other.clone()]),
+        }
     }
 }
 
-fn in_right_order_list(l: &[Packet], r: &[Packet]) -> Option<bool> {
-    let mut i = 0;
-    loop {
-        if i >= l.len() && i >= r.len() {
-            return None;
-        }
-        if i >= l.len() {
-            return Some(true);
-        }
-        if i >= r.len() {
-            return Some(false);
-        }
-        match in_right_order(&l[i], &r[i]) {
-            None => {}
-            Some(b) => return Some(b),
-        }
-        i += 1
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
@@ -61,7 +66,7 @@ fn parse_num(l: &[char]) -> (Packet, &[char]) {
         ret += (*c as u8 - b'0') * off;
         off *= 10;
     }
-    (Packet::Num(ret), &l[i..])
+    (Num(ret), &l[i..])
 }
 
 fn parse_list(l: &[char]) -> (Packet, &[char]) {
@@ -87,7 +92,7 @@ fn parse_list(l: &[char]) -> (Packet, &[char]) {
         }
     }
     debug_assert_eq!(']', l[0]);
-    (Packet::List(v), &l[1..])
+    (List(v), &l[1..])
 }
 
 pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duration> {
@@ -103,25 +108,17 @@ pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duratio
     let part1: usize = input
         .iter()
         .enumerate()
-        .filter(|(_, (l, r))| in_right_order(l, r).unwrap())
+        .filter(|(_, (l, r))| Ordering::Less == l.cmp(r))
         .map(|(i, _)| i + 1)
         .sum();
 
-    let mut input: Vec<Packet> = input.into_iter().flat_map(|(l, r)| [l, r]).collect();
+    let mut input: Vec<_> = input.into_iter().flat_map(|(l, r)| [l, r]).collect();
 
     let d1 = parse("[[2]]");
     let d2 = parse("[[6]]");
     input.push(d1.clone());
     input.push(d2.clone());
-    input.sort_by(|l, r| {
-        if let Some(true) = in_right_order(l, r) {
-            return Ordering::Less;
-        }
-        if let Some(true) = in_right_order(r, l) {
-            return Ordering::Greater;
-        }
-        unreachable!()
-    });
+    input.sort_unstable();
 
     let part2: usize = input
         .into_iter()
@@ -151,12 +148,12 @@ mod tests {
     fn parse_num_test() {
         let input = "42,test".chars().collect::<Vec<_>>();
         let (n, rest) = parse_num(&input);
-        assert_eq!(Packet::Num(42), n);
+        assert_eq!(Num(42), n);
         assert_eq!(",test".to_owned(), rest.iter().collect::<String>());
 
         let input = "42".chars().collect::<Vec<_>>();
         let (n, rest) = parse_num(&input);
-        assert_eq!(Packet::Num(42), n);
+        assert_eq!(Num(42), n);
         assert_eq!("".to_owned(), rest.iter().collect::<String>());
     }
 
@@ -164,7 +161,7 @@ mod tests {
     fn parse_list_test() {
         let input = "[42,13]".chars().collect::<Vec<_>>();
         let (l, rest) = parse_list(&input);
-        assert_eq!(Packet::List(vec![Packet::Num(42), Packet::Num(13),]), l);
+        assert_eq!(List(vec![Num(42), Num(13),]), l);
         assert!(rest.is_empty());
     }
 
@@ -172,13 +169,7 @@ mod tests {
     fn parse_list_nested() {
         let input = "[42,[13,3]]".chars().collect::<Vec<_>>();
         let (l, rest) = parse_list(&input);
-        assert_eq!(
-            Packet::List(vec![
-                Packet::Num(42),
-                Packet::List(vec![Packet::Num(13), Packet::Num(3),]),
-            ]),
-            l
-        );
+        assert_eq!(List(vec![Num(42), List(vec![Num(13), Num(3),]),]), l);
         assert!(rest.is_empty());
     }
 
@@ -187,10 +178,7 @@ mod tests {
         let input = "[[42],[13,3]]".chars().collect::<Vec<_>>();
         let (l, rest) = parse_list(&input);
         assert_eq!(
-            Packet::List(vec![
-                Packet::List(vec![Packet::Num(42)]),
-                Packet::List(vec![Packet::Num(13), Packet::Num(3),]),
-            ]),
+            List(vec![List(vec![Num(42)]), List(vec![Num(13), Num(3),]),]),
             l
         );
         assert!(rest.is_empty());
