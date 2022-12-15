3a6129413107dcb63d6fd7433e3480520964f291
Tomasz Kłak <tomasz@tomaszklak.pl>
2022-12-15 08:52:56 +0100

    day15 speedup with rayon...

--- src/day15.rs
+++ src/day15.rs
@@ -1,60 +1,58 @@
 use anyhow::Result;
 use itertools::Itertools;
-use rustc_hash::FxHashSet as HashSet;
 use smallvec::{smallvec, SmallVec};
+use std::collections::HashSet;
 use std::time::{Duration, Instant};
 
 type V<T> = SmallVec<[T; 8]>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Pos {
-    row: isize,
-    col: isize,
+    row: i64,
+    col: i64,
 }
 
 impl Pos {
-    fn dist(self, other: Self) -> (isize, isize) {
-        ((self.row - other.row).abs(), (self.col - other.col).abs())
+    fn dist(self, other: Self) -> i64 {
+        (self.row - other.row).abs() + (self.col - other.col).abs()
     }
 }
 
-fn sorted_overlap((min1, max1): (isize, isize), (min2, _): (isize, isize)) -> bool {
-    assert!(min1 <= min2);
+fn sorted_overlap((_, max1): (i64, i64), (min2, _): (i64, i64)) -> bool {
     min2 <= max1 || max1 + 1 == min2
 }
 
 fn parse(s: &str) -> (Pos, Pos) {
     let s = s.split(' ').collect_vec();
-    let sx: isize = s[2]
+    let sx: i64 = s[2]
         .strip_prefix("x=")
         .unwrap()
         .strip_suffix(',')
         .unwrap()
         .parse()
         .unwrap();
-    let sy: isize = s[3]
+    let sy: i64 = s[3]
         .strip_prefix("y=")
         .unwrap()
         .strip_suffix(':')
         .unwrap()
         .parse()
         .unwrap();
-    let bx: isize = s[8]
+    let bx: i64 = s[8]
         .strip_prefix("x=")
         .unwrap()
         .strip_suffix(',')
         .unwrap()
         .parse()
         .unwrap();
-    let by: isize = s[9].strip_prefix("y=").unwrap().parse().unwrap();
+    let by: i64 = s[9].strip_prefix("y=").unwrap().parse().unwrap();
     let s = Pos { row: sy, col: sx };
     let b = Pos { row: by, col: bx };
     (s, b)
 }
 
-fn min_max_in_range_for(s: Pos, b: Pos, row: isize) -> Option<(isize, isize)> {
-    let (h, w) = s.dist(b);
-    let dist = h + w;
+fn min_max_in_range_for(s: Pos, b: Pos, row: i64) -> Option<(i64, i64)> {
+    let dist = s.dist(b);
     let off = dist - (s.row - row).abs();
     if off < 0 {
         None
@@ -63,8 +61,8 @@ fn min_max_in_range_for(s: Pos, b: Pos, row: isize) -> Option<(isize, isize)> {
     }
 }
 
-fn intervals_for_row(row: isize, input: &[(Pos, Pos)]) -> V<(isize, isize)> {
-    let mut intervals: V<(isize, isize)> = smallvec![];
+fn intervals_for_row(row: i64, input: &[(Pos, Pos)]) -> V<(i64, i64)> {
+    let mut intervals: V<(i64, i64)> = smallvec![];
     for (s, b) in input {
         if let Some((min, max)) = min_max_in_range_for(*s, *b, row) {
             intervals.push((min, max));
@@ -75,13 +73,13 @@ fn intervals_for_row(row: isize, input: &[(Pos, Pos)]) -> V<(isize, isize)> {
     }
     intervals.sort_unstable_by_key(|(min, _)| *min);
 
-    let mut intervals_merged: V<(isize, isize)> = smallvec![intervals[0]];
+    let mut intervals_merged: V<(i64, i64)> = smallvec![intervals[0]];
     let mut next = 1;
     while next < intervals.len() {
         let last_idx = intervals_merged.len() - 1;
         let last = intervals_merged[last_idx];
         if sorted_overlap(last, intervals[next]) {
-            if intervals[next].1 > intervals_merged[last_idx].1 {
+            if intervals[next].1 > last.1 {
                 intervals_merged[last_idx].1 = intervals[next].1;
             }
         } else {
@@ -92,14 +90,15 @@ fn intervals_for_row(row: isize, input: &[(Pos, Pos)]) -> V<(isize, isize)> {
     intervals_merged
 }
 
-fn find_freq(input: &[(Pos, Pos)], max_row: isize) -> isize {
-    for row in 0..=max_row {
-        let intervals = intervals_for_row(row, input);
-        if intervals.len() > 1 {
-            return (intervals[0].1 + 1) * 4000000 + row;
-        }
-    }
-    unreachable!()
+fn find_freq(input: &[(Pos, Pos)], max_row: i64) -> i64 {
+    use rayon::prelude::*;
+    (0..=max_row)
+        .rev()
+        .par_bridge()
+        .map(|row| (row, intervals_for_row(row, input)))
+        .find_any(|(_, int)| int.len() > 1)
+        .map(|(row, int)| (int[0].1 + 1) * 4000000 + row)
+        .unwrap()
 }
 
 pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duration> {
@@ -112,7 +111,7 @@ pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duratio
     let part1 = intervals_for_row(row, &input)
         .iter()
         .map(|(min, max)| max - min + 1)
-        .sum::<isize>() as usize
+        .sum::<i64>() as usize
         - b_in_row.len();
     let part2 = find_freq(&input, 4000000);
 
