5619e5addeda6ec62a848782b48d41b38b455d1b
Tomasz Kłak <tomasz@tomaszklak.pl>
2022-12-18 12:59:20 +0100

    day18 speedup - switch from flood fill to dijkstra

--- src/day18.rs
+++ src/day18.rs
@@ -1,7 +1,11 @@
 use anyhow::Result;
+use itertools::Itertools;
 use rustc_hash::FxHashSet as HashSet;
-use std::collections::VecDeque;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::time::{Duration, Instant};
+
+type VSet = Vec<Vec<Vec<bool>>>;
 
 const NEIGHBOURS: [P3; 6] = [
     P3 { x: 1, y: 0, z: 0 },
@@ -12,7 +16,7 @@ const NEIGHBOURS: [P3; 6] = [
     P3 { x: 0, y: 0, z: -1 },
 ];
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct P3 {
     x: i32,
     y: i32,
@@ -30,6 +34,9 @@ impl P3 {
             z: self.z + other.z,
         }
     }
+    fn dist(self, other: Self) -> i32 {
+        (self.x - other.x).abs() + (self.y - other.y).abs() + (self.z - other.z).abs()
+    }
 }
 
 fn parse(s: &str) -> P3 {
@@ -41,54 +48,87 @@ fn parse(s: &str) -> P3 {
     }
 }
 
-fn can_grow_for(from: P3, time: usize, taken: &HashSet<P3>) -> bool {
-    let mut taken = taken.clone();
-    let mut todo: VecDeque<P3> = VecDeque::new();
-    todo.push_back(from);
-    let mut c = 0;
-    while let Some(p) = todo.pop_back() {
-        if c > time {
-            break;
-        }
-        taken.insert(p);
-        for n in p.neighbours() {
-            if !taken.contains(&n) {
-                c += 1;
-                todo.push_back(n);
-            }
-        }
+fn get(m: &[Vec<Vec<bool>>], P3 { x, y, z }: P3, default: bool) -> bool {
+    m.get(x as usize)
+        .and_then(|v| v.get(y as usize))
+        .and_then(|v| v.get(z as usize))
+        .copied()
+        .unwrap_or(default)
+}
+
+fn set(m: &mut [Vec<Vec<bool>>], P3 { x, y, z }: P3) {
+    if x < 0 || x as usize >= m.len() {
+        return;
     }
-    c > time
+    if y < 0 || y as usize >= m[x as usize].len() {
+        return;
+    }
+    if z < 0 || z as usize >= m[x as usize][y as usize].len() {
+        return;
+    }
+    m[x as usize][y as usize][z as usize] = true;
 }
 
 fn count_exposed_sides(ps: &[P3]) -> (usize, usize) {
-    let points: HashSet<P3> = ps.iter().copied().collect();
-    let mut out: HashSet<P3> = Default::default();
-    let mut c = 0;
+    let (min_x, max_x) = ps.iter().map(|p| p.x).minmax().into_option().unwrap();
+    let (min_y, max_y) = ps.iter().map(|p| p.y).minmax().into_option().unwrap();
+    let (min_z, max_z) = ps.iter().map(|p| p.z).minmax().into_option().unwrap();
+
+    let mut points =
+        vec![vec![vec![false; 1 + max_z as usize]; 1 + max_y as usize]; 1 + max_x as usize];
+    for p in ps {
+        points[p.x as usize][p.y as usize][p.z as usize] = true;
+    }
+
+    let mut air_around_stones: HashSet<P3> = Default::default();
+    let mut sides_exposed_to_air = 0;
     for p in ps {
         for n in p.neighbours() {
-            if !points.contains(&n) {
-                c += 1;
-                out.insert(n);
+            if !get(&points, n, false) {
+                sides_exposed_to_air += 1;
+                air_around_stones.insert(n);
             }
-        }
-    }
-    let mut inner: HashSet<P3> = Default::default();
-    for p in out {
-        if !can_grow_for(p, 2 * ps.len(), &points) {
-            inner.insert(p);
         }
     }
 
-    let mut c2 = 0;
-    for p in ps {
+    let outside = P3 {
+        x: min_x - 1,
+        y: min_y - 1,
+        z: min_z - 1,
+    };
+
+    let mut inner =
+        vec![vec![vec![false; 1 + max_z as usize]; 1 + max_y as usize]; 1 + max_x as usize];
+    air_around_stones
+        .iter()
+        .filter(|p| !find_path(**p, outside, &points))
+        .for_each(|p| set(&mut inner, *p));
+
+    let without_inner_air = ps
+        .iter()
+        .flat_map(|p| p.neighbours())
+        .filter(|p| !get(&points, *p, false) && !get(&inner, *p, false))
+        .count();
+
+    (sides_exposed_to_air, without_inner_air)
+}
+
+fn find_path(from: P3, to: P3, taken: &VSet) -> bool {
+    let mut todo: BinaryHeap<Reverse<(i32, P3)>> = Default::default();
+    let mut seen = taken.clone();
+    todo.push(Reverse((from.dist(to), from)));
+    while let Some(Reverse((_, p))) = todo.pop() {
+        if p == to {
+            return true;
+        }
         for n in p.neighbours() {
-            if !points.contains(&n) && !inner.contains(&n) {
-                c2 += 1;
+            if !get(&seen, n, false) {
+                set(&mut seen, n);
+                todo.push(Reverse((n.dist(to), n)));
             }
         }
     }
-    (c, c2)
+    false
 }
 
 pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duration> {
