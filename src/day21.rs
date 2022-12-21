54c6ea6105464f29bcad79851127026a37e6b503
Tomasz Kłak <tomasz@tomaszklak.pl>
2022-12-21 16:43:17 +0100

    day21 speedup

--- src/day21.rs
+++ src/day21.rs
@@ -1,19 +1,30 @@
 use anyhow::{Error, Result};
 use itertools::Itertools;
 use rustc_hash::FxHashMap as HashMap;
+use smallvec::SmallVec;
 use std::str::FromStr;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering::SeqCst;
 use std::time::{Duration, Instant};
-use smallvec::{SmallVec};
 
 type V<T> = SmallVec<[T; 10]>;
-type State = HashMap<Node, Expr>;
+type State = Vec<Expr>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct Node(u32);
+struct Node(usize);
+
+static ROOT: AtomicUsize = AtomicUsize::new(0);
+static HUMN: AtomicUsize = AtomicUsize::new(0);
 
 impl Node {
     fn new(s: &str) -> Self {
-        Self(u32::from_le_bytes(s.as_bytes().try_into().unwrap()))
+        Self(u32::from_le_bytes(s.as_bytes().try_into().unwrap()) as usize)
+    }
+}
+
+impl From<&AtomicUsize> for Node {
+    fn from(a: &AtomicUsize) -> Node {
+        Node(a.load(SeqCst))
     }
 }
 
@@ -85,14 +96,15 @@ fn parse(s: &str) -> (Node, Expr) {
 }
 
 fn eval(mut state: State) -> State {
-    let root = Node::new("root");
-    while let Some(Op(_, _, _)) = state.get(&root) {
-        let new_vals : V<(Node, i64)> = state
+    let root = Node::from(&ROOT);
+    while let Op(_, _, _) = state[root.0] {
+        let new_vals: V<(Node, i64)> = state
             .iter()
+            .enumerate()
             .filter_map(|(n, v)| {
                 if let Op(l, op, r) = v {
-                    match (state.get(l), state.get(r)) {
-                        (Some(Const(a)), Some(Const(b))) => Some((*n, op.eval(*a, *b))),
+                    match (state[l.0], state[r.0]) {
+                        (Const(a), Const(b)) => Some((Node(n), op.eval(a, b))),
                         _ => None,
                     }
                 } else {
@@ -103,9 +115,8 @@ fn eval(mut state: State) -> State {
         if new_vals.is_empty() {
             break;
         }
-        println!("{}", new_vals.len());
         for (name, v) in new_vals {
-            state.insert(name, Const(v));
+            state[name.0] = Const(v);
         }
     }
     state
@@ -113,75 +124,54 @@ fn eval(mut state: State) -> State {
 
 fn simplify(mut state: State) -> State {
     state = eval(state);
-    let root = Node::new("root");
+    let root = Node::from(&ROOT);
 
     loop {
-        if let Op(l, Eql, v_name) = *state.get(&root).unwrap() {
-            if let Const(v) = *state.get(&v_name).unwrap() {
-                let l = *state.get(&l).unwrap();
+        if let Op(l, Eql, v_name) = state[root.0] {
+            if let Const(v) = state[v_name.0] {
+                let l = state[l.0];
                 match l {
                     Op(l, Div, r) => {
-                        if let Const(r) = *state.get(&r).unwrap() {
+                        if let Const(r) = state[r.0] {
                             let v = Const(v * r);
-                            state.insert(v_name.to_owned(), v);
-                            state.insert(
-                                Node::new("root"),
-                                Op(l.to_owned(), Eql, v_name.to_owned()),
-                            );
+                            state[v_name.0] = v;
+                            state[root.0] = Op(l.to_owned(), Eql, v_name.to_owned());
                         }
                     }
                     Op(l, Sub, r) => {
-                        if let Const(r) = state.get(&r).unwrap() {
+                        if let Const(r) = state[r.0] {
                             let v = Const(v + r);
-                            state.insert(v_name.to_owned(), v);
-                            state.insert(
-                                Node::new("root"),
-                                Op(l.to_owned(), Eql, v_name.to_owned()),
-                            );
+                            state[v_name.0] = v;
+                            state[root.0] = Op(l.to_owned(), Eql, v_name.to_owned());
                         }
-                        if let Const(l) = state.get(&l).unwrap() {
+                        if let Const(l) = state[l.0] {
                             let v = Const(l - v);
-                            state.insert(v_name.to_owned(), v);
-                            state.insert(
-                                Node::new("root"),
-                                Op(r.to_owned(), Eql, v_name.to_owned()),
-                            );
+                            state[v_name.0] = v;
+                            state[root.0] = Op(r.to_owned(), Eql, v_name.to_owned());
                         }
                     }
                     Op(l, Add, r) => {
-                        if let Const(l) = state.get(&l).unwrap() {
+                        if let Const(l) = state[l.0] {
                             let v = Const(v - l);
-                            state.insert(v_name.to_owned(), v);
-                            state.insert(
-                                Node::new("root"),
-                                Op(r.to_owned(), Eql, v_name.to_owned()),
-                            );
+                            state[v_name.0] = v;
+                            state[root.0] = Op(r.to_owned(), Eql, v_name.to_owned());
                         }
-                        if let Const(r) = state.get(&r).unwrap() {
+                        if let Const(r) = state[r.0] {
                             let v = Const(v - r);
-                            state.insert(v_name.to_owned(), v);
-                            state.insert(
-                                Node::new("root"),
-                                Op(l.to_owned(), Eql, v_name.to_owned()),
-                            );
+                            state[v_name.0] = v;
+                            state[root.0] = Op(l.to_owned(), Eql, v_name.to_owned());
                         }
                     }
                     Op(l, Mul, r) => {
-                        if let Const(l) = state.get(&l).unwrap() {
+                        if let Const(l) = state[l.0] {
                             let v = Const(v / l);
-                            state.insert(v_name.to_owned(), v);
-                            state.insert(
-                                Node::new("root"),
-                                Op(r.to_owned(), Eql, v_name.to_owned()),
-                            );
+                            state[v_name.0] = v;
+                            state[root.0] = Op(r.to_owned(), Eql, v_name.to_owned());
                         }
-                        if let Const(r) = state.get(&r).unwrap() {
+                        if let Const(r) = state[r.0] {
                             let v = Const(v / r);
-                            state.insert(v_name.to_owned(), v);
-                            state.insert(
-                                Node::new("root"),
-                                Op(l.to_owned(), Eql, v_name.to_owned()),
-                            );
+                            state[v_name.0] = v;
+                            state[root.0] = Op(l.to_owned(), Eql, v_name.to_owned());
                         }
                     }
                     _ => {
@@ -194,26 +184,42 @@ fn simplify(mut state: State) -> State {
     state
 }
 
+fn fix(e: Expr, m: &HashMap<Node, usize>) -> Expr {
+    match e {
+        Var => Var,
+        Const(n) => Const(n),
+        Op(l, op, r) => Op(Node(*m.get(&l).unwrap()), op, Node(*m.get(&r).unwrap())),
+    }
+}
+
 pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duration> {
-    let input: State = input.lines().map(parse).collect();
+    let input: Vec<(Node, Expr)> = input.lines().map(parse).collect();
+    let m: HashMap<Node, usize> = input
+        .iter()
+        .enumerate()
+        .map(|(i, (n, _))| (*n, i))
+        .collect();
+    let input: Vec<Expr> = input.into_iter().map(|(_, e)| fix(e, &m)).collect();
 
     let s = Instant::now();
+    ROOT.store(m.get(&Node::new("root")).copied().unwrap(), SeqCst);
+    HUMN.store(m.get(&Node::new("humn")).copied().unwrap(), SeqCst);
 
     let state = eval(input.clone());
-    let part1 = match state.get(&Node::new("root")) {
-        Some(Const(n)) => *n,
+    let part1 = match state[Node::from(&ROOT).0] {
+        Const(n) => n,
         _ => unreachable!(),
     };
 
     let mut state = input;
-    state.insert(Node::new("humn"), Var);
-    state.get_mut(&Node::new("root")).unwrap().make_op(Eql);
+    state[Node::from(&HUMN).0] = Var;
+    state[Node::from(&ROOT).0].make_op(Eql);
     state = simplify(state);
 
-    let part2 = match state.get(&Node::new("root")) {
-        Some(Op(l, Eql, r)) => match (state.get(l), state.get(r)) {
-            (Some(Const(n)), _) => *n,
-            (_, Some(Const(n))) => *n,
+    let part2 = match state[Node::from(&ROOT).0] {
+        Op(l, Eql, r) => match (state[l.0], state[r.0]) {
+            (Const(n), _) => n,
+            (_, Const(n)) => n,
             _ => unreachable!(),
         },
         _ => unreachable!(),
